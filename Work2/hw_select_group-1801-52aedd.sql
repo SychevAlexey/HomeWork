c09db113ef82f25a401691581b2e98aff19ba899
AlexeySychev <ssaann1980@mail.ru>
2021-04-18 19:31:21 +0300

    Update hw_select_group-1801-52aedd.sql
    добавлены 8,9 задания

--- Work2/hw_select_group-1801-52aedd.sql
+++ Work2/hw_select_group-1801-52aedd.sql
@@ -80,12 +80,15 @@ WHERE O.PickingCompletedWhen IS NOT NULL
 		(
 			OL.UnitPrice > 100 OR OL.Quantity > 20
 		)
-ORDER BY	DATEPART(QUARTER, O.OrderDate)	
-		,	CASE	WHEN DATEPART(M, O.OrderDate) BETWEEN 1 AND 4	THEN 1
-					WHEN DATEPART(M, O.OrderDate) BETWEEN 5 AND 8	THEN 2
-					WHEN DATEPART(M, O.OrderDate) BETWEEN 9 AND 12	THEN 3
-			END	
-		,	FORMAT(O.OrderDate, 'd', 'en-gb')
+ORDER BY	Квартал,Треть,ДатаЗаказа
+/*	Сказывается MSSQL 2012 альясы не давал сортировать*/
+
+--ORDER BY	DATEPART(QUARTER, O.OrderDate)	
+--		,	CASE	WHEN DATEPART(M, O.OrderDate) BETWEEN 1 AND 4	THEN 1
+--					WHEN DATEPART(M, O.OrderDate) BETWEEN 5 AND 8	THEN 2
+--					WHEN DATEPART(M, O.OrderDate) BETWEEN 9 AND 12	THEN 3
+--			END	
+--		,	FORMAT(O.OrderDate, 'd', 'en-gb')
 --		ИЛИ по ORDER BY 4,5,2
 /*
 Добавьте вариант этого запроса с постраничной выборкой,
@@ -109,7 +112,8 @@ WHERE O.PickingCompletedWhen IS NOT NULL
 	AND (
 			OL.UnitPrice > 100 OR OL.Quantity > 20
 		)
-ORDER BY 4,5,2
+ORDER BY 	Квартал,Треть,ДатаЗаказа
+--ORDER BY 4,5,2
 OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY
 
 /*
@@ -198,67 +202,15 @@ SELECT 'CustomerTransactions',* FROM [WideWorldImporters].[Sales].[CustomerTrans
 Значит по этой покупке сумма будет (9*240)+(10*13)+(9*13)+(9*13)+(3*25)=2599
 
 */
-
-SELECT	MONTH_ID.YEAR_N				AS	Год
-	,	MONTH_ID.MONTH_N			AS	Месяц
-	,	SUMM_GRP.ОбщКоличТоваров	AS	ОбКолТ
-	,	SUMM_GRP.ОбщаяПродажаЗаМес	AS	ОбПрЗаМес
-	,	SUMM_GRP.СрЧек
-	,	Invoice						AS	КоличЗак
-	,	SR_TOV.SR_TOV				AS	СрЦенаЗа1ТовЗаМес
-FROM 
-	(
-		/*Сделаем табличку с периодами*/
-		SELECT		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
-				,	YEAR(InvoiceDate)							AS	YEAR_N
-				,	MONTH(InvoiceDate)							AS	MONTH_N
-				,	SUM(1)										AS	Invoice --количество заказов
-		FROM		Sales.Invoices 
-		GROUP BY	YEAR(InvoiceDate)*100+MONTH(InvoiceDate) 
-				,	YEAR(InvoiceDate)							
-				,	MONTH(InvoiceDate)
-	)	MONTH_ID
-		LEFT	JOIN (
-							SELECT			YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
-										,	CAST(ROUND(SUM(QNTY),0) AS FLOAT)			AS	ОбщКоличТоваров								
-										,	CAST(ROUND(SUM(UP),0) AS FLOAT)				AS	ОбщаяПродажаЗаМес
-										,	CAST(ROUND(AVG(UP),0) AS FLOAT)				AS	СрЧек
-							FROM	Sales.Invoices AS I													-- Выставленный счет
-										JOIN Sales.Orders AS O ON I.OrderID=O.OrderID					-- Ордер
-										JOIN (
-												SELECT		OrderID,SUM(Quantity*UnitPrice) UP,sum(Quantity) QNTY--,AVG(UnitPrice/Quantity) SR--,AVG(UnitPrice) PRD
-												--INTO #TMP		посчитаем общую сумму заказа / Просуммируем количество / 
-												FROM		Sales.OrderLines
-												--WHERE		OrderID = 57867
-												GROUP BY	OrderID
-												) AS OL ON OL.OrderID=O.OrderID					-- товар
-							--WHERE	i.InvoiceID=55519
-							GROUP BY		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)
-					) SUMM_GRP ON SUMM_GRP.MONTH_ID=MONTH_ID.MONTH_ID
-		LEFT JOIN (
-					SELECT		MONTH_ID
-							,	AVG(SUMMA) SR_TOV --Сделаем среднее значение по уникальным товарам
-					FROM	(
-								SELECT		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
-										,	StockItemID				--	Выведем значение по товару
-										,	AVG(SUMM)	AS SUMMA	--	В разных месяцах оно бывает разное, по этому смотрим по каждому товару в периоде
-								FROM	Sales.Invoices AS I							-- Выставленный счет
-									JOIN Sales.Orders AS O ON I.OrderID=O.OrderID	-- Ордер
-									JOIN (
-											SELECT			OrderID
-														,	StockItemID
-														,	AVG(UnitPrice) SUMM
-											FROM		Sales.OrderLines
-											--WHERE		OrderID = 57867
-											GROUP BY	OrderID
-														,	StockItemID
-											) AS OL ON OL.OrderID=O.OrderID			-- товар
-								GROUP BY	YEAR(InvoiceDate)*100+MONTH(InvoiceDate)
-										,	StockItemID	
-								--ORDER by 1,2
-							)	SR_TOV
-					GROUP BY	MONTH_ID
-					) SR_TOV ON SR_TOV.MONTH_ID = MONTH_ID.MONTH_ID
+--	Вот всегда так, когда не понимаю поставленной задачи, начинаю изобретать, а то вдруг я угадаю. Скопировал ваш вариант
+SELECT	YEAR(o.OrderDate) as [Year]		
+	,	MONTH(o.OrderDate) as [Month]	
+	,	AVG(ol.UnitPrice) as [Averege]
+	,	SUM(ol.Quantity*ol.UnitPrice) as [Summa] 
+FROM Sales.Invoices i
+LEFT JOIN Sales.Orders o on i.OrderID = o.OrderID
+LEFT JOIN Sales.OrderLines ol on o.OrderID = ol.OrderID
+GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
 
 /*
 8. Отобразить все месяцы, где общая сумма продаж превысила 10 000
@@ -271,31 +223,155 @@ FROM
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-TODO: напишите здесь свое решение
+SELECT	[Year]		
+	,	[Month]	
+	,	ISNULL(SUMMA,0)		as [Summa] 
+FROM 
+	(	--	Создадим табличку с периодом, если будет отсутствовать в месяце сумма, в выборке будет стоять 0
+		SELECT		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
+		FROM		Sales.Invoices 
+		GROUP BY	YEAR(InvoiceDate)*100+MONTH(InvoiceDate) 
+	) IST
+		LEFT JOIN ( -- Вытащим суммы с продажами за месяц
+					
+					SELECT  YEAR(o.OrderDate)*100+MONTH(o.OrderDate) MONTH_ID
+						,	YEAR(o.OrderDate) as [Year]		
+						,	MONTH(o.OrderDate) as [Month]	
+						,	SUM(ol.Quantity*ol.UnitPrice) as [Summa] 
+					FROM Sales.Invoices i
+					LEFT JOIN Sales.Orders o on i.OrderID = o.OrderID
+					LEFT JOIN Sales.OrderLines ol on o.OrderID = ol.OrderID
+					GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
+					HAVING SUM(ol.Quantity*ol.UnitPrice) > 10000
+				
+				) M ON M.MONTH_ID = IST.MONTH_ID
+ORDER BY [Year]		
+	,	[Month]	
 
 /*
-9. Вывести сумму продаж, дату первой продажи
-и количество проданного по месяцам, по товарам,
-продажи которых менее 50 ед в месяц.
+9. 
+  Вывести сумму продаж	
+, дату первой продажи и количество проданного по месяцам 
+, по товарам 
+, продажи которых менее 50 ед в месяц.
+
 Группировка должна быть по году,  месяцу, товару.
 
 Вывести:
 * Год продажи
 * Месяц продажи
-* Наименование товара
-* Сумма продаж
-* Дата первой продажи
-* Количество проданного
+* Наименование товара	-- Все товары проданные за месяц
+* Количество проданного	-- За месяц
+* Сумма продаж			-- За месяц этого товара
+* Дата первой продажи	-- Первая продажа товара
+
 
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-TODO: напишите здесь свое решение
-
+SELECT		IST.Y							AS год
+		,	IST.M							AS Месяц
+		,	ISNULL(PRODUCT,'----------')	As Товар
+		,	ISNULL(QNTY,0)					AS Количество
+		,	ISNULL(Summa,0)					AS СуммаПродЗаМес
+		,	ISNULL(SaleDate,'01/01/0001')	AS МинДатаПродажи
+FROM 
+	(	--	Создадим табличку с периодом
+		SELECT			YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
+					,	YEAR(InvoiceDate)	AS Y
+					,	MONTH(InvoiceDate)	AS M
+		FROM		Sales.Invoices 
+		GROUP BY		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)
+					,	YEAR(InvoiceDate)	
+					,	MONTH(InvoiceDate)
+	) IST
+		LEFT JOIN ( -- Вытащим суммы с продажами за месяц
+					SELECT  YEAR(o.OrderDate)*100+MONTH(o.OrderDate)	AS	MONTH_ID	--	Идентификатор
+						,	MIN(SC.TransactionDate)						AS	SaleDate	--	Дата первой продажи товара
+						,	ol.DESCRIPTION								AS	PRODUCT		--	Товар
+						,	SUM(ol.Quantity)							AS	QNTY		--	Количесво
+						,	SUM(ol.Quantity*ol.UnitPrice)				as	Summa		--	Продаж за месяц
+					FROM Sales.Invoices i
+					JOIN Sales.Orders o on i.OrderID = o.OrderID
+					JOIN Sales.OrderLines ol on o.OrderID = ol.OrderID
+					JOIN [WideWorldImporters].[Sales].[CustomerTransactions] SC ON SC.InvoiceID = I.InvoiceID
+					GROUP BY YEAR(o.OrderDate)*100+MONTH(o.OrderDate)					
+						,	ol.DESCRIPTION								
+					HAVING SUM(ol.Quantity) <= 50
+--					ORDER BY 1
+				) M ON M.MONTH_ID = IST.MONTH_ID
+ORDER BY	год
+		,	Месяц
+		,	Товар
 -- ---------------------------------------------------------------------------
 -- Опционально
 -- ---------------------------------------------------------------------------
 /*
 Написать запросы 8-9 так, чтобы если в каком-то месяце не было продаж,
 то этот месяц также отображался бы в результатах, но там были нули.
+*/
+
+/*
+Есть у меня такая болячка, когда не понимаю задачи начинаю додумывать
+
+SELECT	MONTH_ID.YEAR_N				AS	Год
+		,	MONTH_ID.MONTH_N			AS	Месяц
+		,	SUMM_GRP.ОбщКоличТоваров	AS	ОбКолТ
+		,	SUMM_GRP.ОбщаяПродажаЗаМес	AS	ОбПрЗаМес
+		,	SUMM_GRP.СрЧек
+		,	Invoice						AS	КоличЗак
+		,	SR_TOV.SR_TOV				AS	СрЦенаЗа1ТовЗаМес
+	FROM 
+		(
+			/*Сделаем табличку с периодами*/
+			SELECT		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
+					,	YEAR(InvoiceDate)							AS	YEAR_N
+					,	MONTH(InvoiceDate)							AS	MONTH_N
+					,	SUM(1)										AS	Invoice --количество заказов
+			FROM		Sales.Invoices 
+			GROUP BY	YEAR(InvoiceDate)*100+MONTH(InvoiceDate) 
+					,	YEAR(InvoiceDate)							
+					,	MONTH(InvoiceDate)
+		)	MONTH_ID
+			LEFT	JOIN (
+								SELECT			YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
+											,	CAST(ROUND(SUM(QNTY),0) AS FLOAT)			AS	ОбщКоличТоваров								
+											,	CAST(ROUND(SUM(UP),0) AS FLOAT)				AS	ОбщаяПродажаЗаМес
+											,	CAST(ROUND(AVG(UP),0) AS FLOAT)				AS	СрЧек
+								FROM	Sales.Invoices AS I													-- Выставленный счет
+											JOIN Sales.Orders AS O ON I.OrderID=O.OrderID					-- Ордер
+											JOIN (
+													SELECT		OrderID,SUM(Quantity*UnitPrice) UP,sum(Quantity) QNTY--,AVG(UnitPrice/Quantity) SR--,AVG(UnitPrice) PRD
+													--INTO #TMP		посчитаем общую сумму заказа / Просуммируем количество / 
+													FROM		Sales.OrderLines
+													--WHERE		OrderID = 57867
+													GROUP BY	OrderID
+													) AS OL ON OL.OrderID=O.OrderID					-- товар
+								--WHERE	i.InvoiceID=55519
+								GROUP BY		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)
+						) SUMM_GRP ON SUMM_GRP.MONTH_ID=MONTH_ID.MONTH_ID
+			LEFT JOIN (
+						SELECT		MONTH_ID
+								,	AVG(SUMMA) SR_TOV --Сделаем среднее значение по уникальным товарам
+						FROM	(
+									SELECT		YEAR(InvoiceDate)*100+MONTH(InvoiceDate)	AS	MONTH_ID
+											,	StockItemID				--	Выведем значение по товару
+											,	AVG(SUMM)	AS SUMMA	--	В разных месяцах оно бывает разное, по этому смотрим по каждому товару в периоде
+									FROM	Sales.Invoices AS I							-- Выставленный счет
+										JOIN Sales.Orders AS O ON I.OrderID=O.OrderID	-- Ордер
+										JOIN (
+												SELECT			OrderID
+															,	StockItemID
+															,	AVG(UnitPrice) SUMM
+												FROM		Sales.OrderLines
+												--WHERE		OrderID = 57867
+												GROUP BY	OrderID
+															,	StockItemID
+												) AS OL ON OL.OrderID=O.OrderID			-- товар
+									GROUP BY	YEAR(InvoiceDate)*100+MONTH(InvoiceDate)
+											,	StockItemID	
+									--ORDER by 1,2
+								)	SR_TOV
+						GROUP BY	MONTH_ID
+						) SR_TOV ON SR_TOV.MONTH_ID = MONTH_ID.MONTH_ID
 */
