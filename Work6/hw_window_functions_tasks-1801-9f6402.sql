a3aed2210f38ea2ccf5e1fdbff85b5dc179577de
AlexeySychev <ssaann1980@mail.ru>
2021-04-25 22:57:00 +0300

    Update hw_window_functions_tasks-1801-9f6402.sql

--- Work6/hw_window_functions_tasks-1801-9f6402.sql
+++ Work6/hw_window_functions_tasks-1801-9f6402.sql
@@ -37,22 +37,84 @@ USE WideWorldImporters
 Продажи можно взять из таблицы Invoices.
 Нарастающий итог должен быть без оконной функции.
 */
+set statistics time, io on;
+SELECT		i.InvoiceID
+		,	c.CustomerName
+		,	i.InvoiceDate 
+		,	(
+			    SELECT SUM(il.Quantity * il.UnitPrice)
+			    FROM Sales.InvoiceLines AS il
+			    WHERE il.InvoiceID = i.InvoiceID
+			)	AS	[InvoceSum]
+		,	(
+				SELECT SUM(il2.Quantity * il2.UnitPrice)
+				FROM Sales.InvoiceLines AS il2
+				    JOIN Sales.Invoices AS i2 ON il2.InvoiceID = i2.InvoiceID
+				WHERE i2.InvoiceDate <= EOMONTH(i.InvoiceDate)
+				  AND YEAR(i2.InvoiceDate) = 2015
+			)	AS [InvoceSumMonth]
+FROM Sales.Invoices AS i
+    JOIN Sales.Customers c ON i.CustomerID = c.CustomerID
+		JOIN Sales.InvoiceLines il ON i.InvoiceID = il.InvoiceID
+WHERE YEAR(i.InvoiceDate) = 2015
+ORDER BY i.InvoiceDate, i.CUstomerID;
 
-напишите здесь свое решение
+--(затронуто строк: 71898)
+-- Время работы SQL Server:
+--   Время ЦП = 27312 мс, затраченное время = 28556 мс.
+
 
 /*
 2. Сделайте расчет суммы нарастающим итогом в предыдущем запросе с помощью оконной функции.
    Сравните производительность запросов 1 и 2 с помощью set statistics time, io on
 */
+set statistics time, io on;
+SELECT
+		i.InvoiceID AS [InvoiceId]
+	,	c.CustomerName AS [CustomerName]
+	,	i.InvoiceDate AS [InvoiceDate]
+	,	SUM(il.Quantity * il.UnitPrice) OVER (PARTITION BY i.InvoiceID) [InvoceSum]
+	,	SUM(il.Quantity * il.UnitPrice) OVER (ORDER BY DATEPART(YEAR, i.InvoiceDate), DATEPART(MONTH, i.InvoiceDate)) [InvoceSumMonth]
+FROM Sales.Invoices i
+		JOIN Sales.Customers c ON c.CustomerID = i.CustomerID
+		JOIN Sales.InvoiceLines il ON il.InvoiceID = i.InvoiceID
+WHERE YEAR(i.InvoiceDate) = 2015
+ORDER BY i.InvoiceDate, i.CustomerID;
 
-напишите здесь свое решение
+--
+-- Время работы SQL Server:
+--   Время ЦП = 219 мс, затраченное время = 3520 мс.
 
 /*
 3. Вывести список 2х самых популярных продуктов (по количеству проданных) 
 в каждом месяце за 2016 год (по 2 самых популярных продукта в каждом месяце).
 */
 
-напишите здесь свое решение
+WITH MonthSales
+AS (	-- Вытащим все товары и поставим количество по месяцам
+    SELECT si.StockItemName
+			,	SUM(il.Quantity)		AS [QNTY]
+			,	MONTH(i.InvoiceDate)	AS [Month]
+    FROM Sales.InvoiceLines il
+			JOIN [Sales].[Invoices] i ON il.InvoiceID = i.InvoiceID 
+			JOIN Warehouse.StockItems si ON il.StockItemID = si.StockItemID
+	WHERE YEAR(i.InvoiceDate) = 2016
+    GROUP BY si.StockItemName, MONTH(i.InvoiceDate)
+),
+SalesNum
+AS (	-- отсортируем товары по сумме и месяцу + пронумеруем
+    SELECT		MonthSales.StockItemName 
+			,	MonthSales.QNTY
+			,	MonthSales.[Month]
+			,	ROW_NUMBER() OVER(PARTITION BY MonthSales.[Month] ORDER BY MonthSales.QNTY DESC) AS [RN]
+    FROM MonthSales
+)	-- выберем больше или равное 2
+	SELECT		Itog.StockItemName	AS Товар	
+			,	[Month]				AS Месяц
+			,	Itog.QNTY			AS Колич
+	FROM	SalesNum Itog
+	WHERE Itog.RN <= 2
+	ORDER BY Itog.[Month], Itog.QNTY DESC;
 
 /*
 4. Функции одним запросом
@@ -68,20 +130,72 @@ USE WideWorldImporters
 Для этой задачи НЕ нужно писать аналог без аналитических функций.
 */
 
-напишите здесь свое решение
+SELECT		si.StockItemID																			AS	ИдТовара
+		,	si.StockItemName 																		AS	Наименование
+		,	si.Brand																				AS	Бренд
+		,	ROW_NUMBER() OVER(PARTITION BY LEFT(si.StockItemName, 1) ORDER BY si.StockItemName) 	AS	НумерацияПоПервомуСимволу
+		,	COUNT(*) OVER()																			AS	ОбщееКоличСтрок
+		,	COUNT(*) OVER(PARTITION BY LEFT(si.StockItemName, 1)) 									AS	ОщееКоличПоПервомуСимволу
+		,	LEAD(si.StockItemID) OVER(ORDER BY si.StockItemName)									AS	ИдСледующий
+		,	LAG(si.StockItemID) OVER(ORDER BY si.StockItemName)			 							AS	ИдПредыдущий
+		,	LAG(si.StockItemName, 2, 'No items') OVER(ORDER BY si.StockItemName)					AS	НаименТовара2назад
+		,	NTILE(30) OVER(ORDER BY si.TypicalWeightPerUnit)										AS	ГруппаИз30поВесу
+FROM Warehouse.StockItems si
+ORDER BY si.StockItemName;
 
 /*
 5. По каждому сотруднику выведите последнего клиента, которому сотрудник что-то продал.
    В результатах должны быть ид и фамилия сотрудника, ид и название клиента, дата продажи, сумму сделки.
 */
 
-напишите здесь свое решение
+SELECT		p.PersonID
+       ,	p.FullName 
+       ,	c.CustomerID
+       ,	c.CustomerName
+       ,	r.TransactionDate
+       ,	r.TransactionAmount
+FROM
+(
+    SELECT		ct.CustomerID
+           ,	i.SalespersonPersonID
+           ,	ct.TransactionDate
+           ,	ct.TransactionAmount
+           ,	ROW_NUMBER() OVER(PARTITION BY SalespersonPersonID ORDER BY TransactionDate DESC) AS RM
+    FROM Sales.CustomerTransactions ct
+         INNER JOIN Sales.Invoices i ON ct.InvoiceID = i.InvoiceID
+) AS r
+	JOIN Application.People p ON r.SalespersonPersonID = p.PersonID
+	JOIN Sales.Customers c ON r.CustomerID = c.CustomerID
+WHERE r.RM = 1;
 
 /*
 6. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
 В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки.
 */
+---	Сделаю выборку по уникальности дорогих товаров, что бы не показывались два одинаковых дорогих товара
+SELECT		c.CustomerID
+		,	c.CustomerName
+		,	subq.StockItemID
+		,	subq.StockItemName
+		,	subq.UnitPrice
+		,	subq.DateInVoice
+FROM
+(
+    SELECT		i.CustomerID
+           ,	il.StockItemID
+           ,	si.StockItemName
+           ,	si.UnitPrice
+           ,	MAX(i.InvoiceDate) DateInVoice
+           ,	ROW_NUMBER() OVER(PARTITION BY i.CustomerID ORDER BY si.UnitPrice DESC) AS [RN]
+    FROM Sales.InvoiceLines il
+         JOIN Sales.Invoices i ON il.InvoiceID = i.InvoiceID
+         JOIN Warehouse.StockItems si ON il.StockItemID = si.StockItemID
+	GROUP BY i.CustomerID
+           ,	il.StockItemID
+           ,	si.StockItemName
+           ,	si.UnitPrice
+) AS subq
+	JOIN Sales.Customers c ON subq.CustomerID = c.CustomerID
+WHERE subq.RN <= 2;
 
-напишите здесь свое решение
-
-Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
+--Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
